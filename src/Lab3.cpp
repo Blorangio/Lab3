a55cf2bf0be7c5615fad1d61e06f8608cd50d7a9
Fenton Pratt <fadpratt@gmail.com>
2023-06-27 11:17:57 -0500

    Initial Commit

---
+++ src/Lab3.cpp
@@ -0,0 +1,18 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/admin/Documents/CTD_2023/Labs/Lab3/src/Lab3.ino"
+void setup();
+void loop();
+#line 1 "/Users/admin/Documents/CTD_2023/Labs/Lab3/src/Lab3.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+void setup() {
+  pinMode(A0, OUTPUT);
+}
+void loop() {
+  
+
+}
---
+++ src/Lab3.ino
@@ -0,0 +1,9 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+void setup() {
+  pinMode(A0, OUTPUT);
+}
+void loop() {
+  
+
+}
